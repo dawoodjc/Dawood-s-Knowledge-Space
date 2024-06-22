2706e7a59b0a56977aa35c5d38d72ecd6b564b0e
Dawood Cheema <dawood.j.cheema@gmail.com>
2024-06-22 14:02:10 +0500

    Create fraction addition code

---
+++ python/20240620-adding-fractions.py
@@ -0,0 +1,14 @@
+print("Enter numirator")
+x=input()
+print("Enter Denominator")
+y=input()
+print("Entor numinator")
+l=input()
+print("Enter Denominator")
+f=input()
+if(int(f)==int(y)):
+  print(int(x)+int(l))
+  print("__")
+  print(f)
+else:
+  print("Please only enter same denominator")
